5dfd2bc0cd83409a72e2d9cd6a114f168de13765
Christina Kaliora <crhstina.kaliora@pirron.one>
2020-06-12 09:36:54 +0200

    ring buffer is passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current_index = 0
+        self.buffer = []
 
     def append(self, item):
-        pass
+        # if buffer is not full
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(item)
+        # Otherwise, set the value of buffer at its current index to item
+        self.buffer[self.current_index] = item
+        # then increment current index
+        self.current_index += 1
+        # then check to make sure you haven't reached the max of the buffer
+        if self.current_index > (self.capacity - 1):
+        # if you have exceeded the max, set the current index back to 0, you've reached the beginning of the ring
+            self.current_index = 0
 
     def get(self):
-        pass
+        return self.buffer
